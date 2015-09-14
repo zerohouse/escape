798ed9cc3c0ae33ad03db9581e81a2ee91187cc3
dev <parksungho86@gmail.com>
2015-09-14 11:32:34 +0900

    update

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,5 @@
-module.exports = function(app, logger){
-app.get('/api/abc', function (req, res) {
-    res.send('weaasfwweef');
-});
+module.exports = function (app) {
+    app.get('/api/abc', function (req, res) {
+        res.send('weaasfwweef');
+    });
 };
