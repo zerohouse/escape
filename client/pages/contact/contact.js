edbd6c8306bf2e272f6257153fdd12c79d3fea6e
dev <parksungho86@gmail.com>
2015-10-04 22:20:17 +0900

    ngin

--- client/pages/contact/contact.js
+++ client/pages/contact/contact.js
@@ -1,16 +1,21 @@
 app.controller('contact', function () {
     var oPoint = new nhn.api.map.LatLng(35.1359970, 129.1000470);
     nhn.api.map.setDefaultPoint('LatLng');
-    oMap = new nhn.api.map.Map('tmap' ,{
-        point : oPoint,
-        zoom : 10,
-        enableWheelZoom : true,
-        enableDragPan : true,
-        enableDblClickZoom : false,
-        mapMode : 0,
-        activateTrafficMap : false,
-        activateBicycleMap : false,
-        minMaxLevel : [ 1, 14 ],
-        size : new nhn.api.map.Size(500, 400)
+    var oMap = new nhn.api.map.Map('tmap', {
+        point: oPoint,
+        zoom: 10,
+        enableWheelZoom: true,
+        enableDragPan: true,
+        enableDblClickZoom: false,
+        mapMode: 0,
+        activateTrafficMap: false,
+        activateBicycleMap: false,
+        minMaxLevel: [1, 14],
+        size: new nhn.api.map.Size(500, 400)
     });
+
+    var oMarker = new nhn.api.map.Marker(oIcon, {title: 'SIXTY ESCAPE'});
+    oMarker.setPoint(oPoint);
+    oMap.addOverlay(oMarker);
+
 });
