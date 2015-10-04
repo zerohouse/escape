1272703da3923c672d677bde1421b1426a725690
dev <parksungho86@gmail.com>
2015-10-04 19:20:26 +0900

    ngin

--- client/pages/main/main.js
+++ client/pages/main/main.js
@@ -2,4 +2,13 @@ app.controller('main', function ($scope) {
 
     $scope.name = "main";
 
+    var slides = $scope.slides = [
+        {
+            image: '/dist/lib/main_back1.jpg'
+        },
+        {
+            image: '/dist/lib/main_back2.jpg'
+        }
+    ];
+
 });
